80e27896a9e16d4780b57596536c2eacefc7053a
jziff <jziff@umich.edu>
2024-05-22 15:54:45 -0700

    adding experience links + adjusting name animation

--- js/name.js
+++ js/name.js
@@ -16,18 +16,18 @@ function delay(milliseconds){
 }
 
 window.addEventListener("load", async function() {
-    if (!isReduced) {
-        n = 150;
-        heading.innerHTML = "_";
-        str = "Jacob Ziff"
+    // if (!isReduced) {
+    n = 150;
+    heading.innerHTML = "_";
+    str = "Jacob Ziff"
 
-        for (i = 0; i < str.length - 1; ++i) {
-            await delay(n);
-            partial = str.slice(0, i + 1);
-            heading.innerHTML = partial.concat("_");
-        }
-
+    for (i = 0; i < str.length - 1; ++i) {
         await delay(n);
-        heading.innerHTML = str;
+        partial = str.slice(0, i + 1);
+        heading.innerHTML = partial.concat("_");
     }
+
+    await delay(n);
+    heading.innerHTML = str;
+    // }
 });
